452818ba7d4cfaafbf5d483f6b8c60641fafb534
Rafael Aquino <orfaquino@outlook.com>
2023-08-28 16:46:37 -0300

    primeiro commit

---
+++ String.py
@@ -0,0 +1,26 @@
+#colocar em maiusculo a string
+
+nome = "rafael"
+print(nome.upper())
+
+#colocar a string minuscula
+
+nome2 = "RAFAEL"
+print(nome.lower())
+
+#colocar a string em lista
+
+nome3 = "Rafael Oliveira"
+print(nome3.split())
+
+#slice de string
+
+print(nome3[0:4])
+print(nome3[0:4].upper())
+
+#inverter o elemento
+print(nome3[::-1])
+
+#substituir o elemento R por g
+print(nome3.replace('R', 'g'))
+
